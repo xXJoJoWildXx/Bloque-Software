153e17731b69e44e1b325459d93fccae74bdd348
Jorge Blásquez <a01637706@tec.mx>
2024-04-10 09:52:28 -0600

    CSS a Full

--- back/app.js
+++ back/app.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+
+//Middlewares
+app.use(cors())
+app.use(bodyParser.json())
 
 app.post('/api/test', (req, res) => {
     res.json({
         greeting: "hello world!"
     });
 });
+
+app.post('/api/customGreeting', (req, res) => {
+    const name = req.body.name;
+    
+    res.json({
+        greeting: `Hello ${name}!`
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
--- front/js/script.js
+++ front/js/script.js
@@ -9,3 +9,21 @@ let counterValue = 0;
 document.getElementById('incrementCounter').addEventListener('click', function(){
     document.getElementById('incrementCounter').innerHTML = counterValue ++;
 });
+
+//Form function to send the name to the API Server
+document.getElementById('nameForm').addEventListener('submit', function(event){
+    event.preventDefault()
+    const name = document.getElementById('name').value
+
+    fetch('http://localhost:3000/api/customGreeting', {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json",
+        },
+        body: JSON.stringify({name : name})
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('serverResponse').innerHTML = data.greeting
+    })
+})
